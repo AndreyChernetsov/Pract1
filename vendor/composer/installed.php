bf064497239ac82d1e46bb12a1fae7b0b2bb8ad4
AndreyChernetsov <andcher24@gmail.com>
2024-03-12 20:21:48 +0700

    Настройка автозагрузки классов

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'andrey/htdohs',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dcdd835e3f3041e5c0cc38ce91e40edaa50ed9a7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'andrey/htdohs' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dcdd835e3f3041e5c0cc38ce91e40edaa50ed9a7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
