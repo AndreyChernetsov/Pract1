d532ad8b575e8385e7580d95468940f339eaf190
AndreyChernetsov <146418051+AndreyChernetsov@users.noreply.github.com>
2024-03-21 01:13:01 +0700

    Исправление ошибки

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -27,9 +27,9 @@ class Site
       if ($request->method === 'POST') {
    
           $validator = new Validator($request->all(), [
-              'name' => ['required'],
-              'login' => ['required', 'unique:users,login'],
-              'password' => ['required']
+              'login' => ['required'],
+              'password' => ['required', 'unique:users,password'],
+              'role' => ['required']
           ], [
               'required' => 'Поле :field пусто',
               'unique' => 'Поле :field должно быть уникально'
