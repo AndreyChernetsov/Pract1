aa027818efc4680d24373c4558b2c90420edfe7c
AndreyChernetsov <146418051+AndreyChernetsov@users.noreply.github.com>
2024-03-20 18:41:13 +0700

    Базовая защита от CSRF-атак

--- config/app.php
+++ config/app.php
@@ -23,5 +23,10 @@ return [
     'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
    ],
  
+   'routeAppMiddleware' => [
+    'csrf' => \Middlewares\CSRFMiddleware::class,
+    'trim' => \Middlewares\TrimMiddleware::class,
+    'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+   ],
  
 ];
--- views/site/login.php
+++ views/site/login.php
@@ -1,16 +1,15 @@
-<h2>Вход</h2>
+<h2>Авторизация</h2>
 <h3><?= $message ?? ''; ?></h3>
 
 <h3><?= app()->auth->user()->name ?? ''; ?></h3>
 <?php
 if (!app()->auth::check()):
    ?>
-   <div>
-        <form method="post">
-            <label>Логин <input type="text" name="login"></label>
-            <label>Пароль <input type="password" name="password"></label>
-            <label>Роль <input type="text" name="role"></label>
-            <button>Войти</button>
-        </form>
-   </div>
+   <form method="post">
+       <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
+       <label>Логин <input type="text" name="login"></label>
+       <label>Пароль <input type="password" name="password"></label>
+       <label>Роль <input type="text" name="role"></label>
+       <button>Войти</button>
+   </form>
 <?php endif;
